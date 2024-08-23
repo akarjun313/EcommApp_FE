cc291cf5da3bd809dbe7b52bac3e1a78e6593551
Arjun A <akarjun313@gmail.com>
2024-08-23 12:17:23 +0530

    changed to railway

--- src/baseURL/baseURL.js
+++ src/baseURL/baseURL.js
@@ -1,2 +1,3 @@
 // export const baseUrl = 'http://localhost:5213'
-export const baseUrl = 'ecomm-app-be.vercel.app'
+// export const baseUrl = 'ecomm-app-be.vercel.app' 
+export const baseUrl = 'ecommappbe-production.up.railway.app'
